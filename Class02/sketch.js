40e78fdcf9b97c8a295b48bb3a585730ed74865d
Adam <n00211418@iadt.ie>
2023-02-03 14:55:28 +0000

    complete class 2

---
+++ Class02/sketch.js
@@ -0,0 +1,65 @@
+
+
+let data = [12, 100, 150, 90, 200]
+let numBlocks = data.length;
+
+let chartWidth = 300;
+let chartHeight = 400;
+let marginLeft = 20;
+let marginRight = 5;
+let blockGap = 10;
+let screenWidth = 500;
+let screenHeight = 500;
+let blockWidth = (chartWidth - (marginLeft + marginRight) - ((numBlocks - 1) * blockGap)) / numBlocks;
+let firstBlockxPos = ((screenWidth - chartWidth) / 2) + marginLeft;
+let masterGap = blockWidth + blockGap
+
+
+// function squarer(_num) {
+//   let squared = _num * _num
+//   return squared
+// }
+
+function scaleChart(arr) {
+  // console.log(Math.max(...arr))
+  let maxValue = Math.max(...arr);
+  let scaleValue = chartHeight / maxValue;
+  let final = []
+
+  for(let i = 0; i < arr.length; i++) {
+    final.push(arr[i] * scaleValue)
+  }
+
+  console.log(final)
+  return final
+}
+
+// scaleChart([1, 2, 3, 66, 7, 90])
+// scaleChart([100, 300, 200, 150, 200, 10000])
+// scaleChart([900, 200, 400, 150, 300, 700])
+// scaleChart([50, 60, 80, 10, 20, 100])
+scaleChart([1, 2, 3, 4, 5, 6])
+let scaleData = scaleChart(data)
+
+function setup() {
+  createCanvas(screenWidth, screenHeight);
+  background(200);
+  angleMode(DEGREES);
+  rectMode(CORNER);
+  noLoop();
+}
+
+function draw() {
+  // makes all random() calls == 40
+  // randomSeed(40)
+  background(200);
+  fill(0);
+  for (let i = 0; i < numBlocks; i++) {
+    push();
+    translate(firstBlockxPos + (i * masterGap), 450)
+    fill(scaleData[i] / 2, 0, 0)
+    rect(0, 0, blockWidth, -scaleData[i]);
+    pop();
+  }
+
+}
