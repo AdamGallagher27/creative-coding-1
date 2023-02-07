afde5794e749b912f507fe6c08bb91c60a4b8836
Adam Gallagher (Student) <N00211418@iadt.ie>
2023-02-07 16:56:36 +0000

    introduced objects

--- Class03/sketch.js
+++ Class03/sketch.js
@@ -1,43 +1,42 @@
 
 
-let data = [12, 100, 150, 90, 200, 100, 12 , 200, 35, 300]
-let numBlocks = data.length;
+// let data = [12, 100, 150, 90, 200, 100, 12 , 200, 35, 300]
 
-let chartWidth = 300;
-let chartHeight = 400;
+// data as array of objects 
+let fruits = [
+  {name:"bananas", sales:100},
+  {name:"apples", sales:200},
+  {name:"oranges", sales:300},
+  {name:"grapes", sales:200},
+  {name:"pears", sales:150},
+]
+
+// let numSales = []
+// fruits.forEach(element => numSales.push(element.sales))
+
+
+// global variables
+let numBlocks = fruits.length;
+let chartWidth = 200;
+let chartHeight = 300;
+let chartX = 100
+let chartY = 450
 let marginLeft = 20;
-let marginRight = 5;
-let blockGap = 10;
+let marginRight = 20;
+let blockGap = 5;
 let screenWidth = 500;
 let screenHeight = 500;
+let chartTicks = 5;
+let tickLength = 5;
+
+// calculations from globals
 let blockWidth = (chartWidth - (marginLeft + marginRight) - ((numBlocks - 1) * blockGap)) / numBlocks;
-let firstBlockxPos = ((screenWidth - chartWidth) / 2) + marginLeft;
 let masterGap = blockWidth + blockGap
+let maxValue = Math.max(...fruits.map( obj => obj.sales ));
 
-
-// function scaleChart(arr) {
-//   // console.log(Math.max(...arr))
-//   let maxValue = Math.max(...arr);
-//   let scaleValue = chartHeight / maxValue;
-//   let final = []
-
-//   for(let i = 0; i < arr.length; i++) {
-//     final.push(arr[i] * scaleValue)
-//   }
-
-//   console.log(final)
-//   return final
-// }
-
-// scaleChart([1, 2, 3, 66, 7, 90])
-// scaleChart([100, 300, 200, 150, 200, 10000])
-// scaleChart([900, 200, 400, 150, 300, 700])
-// scaleChart([50, 60, 80, 10, 20, 100])
-// scaleChart([1, 2, 3, 4, 5, 6])
-// let scaleData = scaleChart(data)
 
 function scaleMe(num) {
-  let maxValue = Math.max(...data);
+  let maxValue = Math.max(...fruits.map( obj => obj.sales ));
   let scaleValue = chartHeight / maxValue;
 
   return num * scaleValue
@@ -56,12 +55,35 @@ function draw() {
   // randomSeed(40)
   background(200);
   fill(0);
+
+  // translates the whole chart
+  translate(chartX, chartY)
+
+  // loop draws bars
   for (let i = 0; i < numBlocks; i++) {
     push();
-    translate(firstBlockxPos + (i * masterGap), 450)
-    fill(data[i], 0, 0)
-    rect(0, 0, blockWidth, -scaleMe(data[i]));
+    translate(marginLeft + (i * masterGap), 0)
+    fill(fruits[i].sales, 0, 0)
+    rect(0, 0, blockWidth, scaleMe(-fruits[i].sales));
     pop();
+  }
+
+  // draws X axis
+  // draw black line horizontally
+  stroke(0)
+  strokeWeight(1)
+
+  let tGap = chartHeight / (chartTicks - 1)
+  let numGap = maxValue / (chartTicks)
+  
+  line(0, 0, chartWidth, 0)
+  line(0, 0, 0, -chartHeight)
+
+  for(let x = 0; x < chartTicks; x++) {
+    line(0, x*-tGap, -tickLength, -x*tGap)
+    textSize(15)
+    textAlign(RIGHT, CENTER)
+    text(x*tGap.toFixed(2), -10, x*-tGap)
   }
 
 }
