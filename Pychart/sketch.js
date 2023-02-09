1b2b330411717450ffdb596a8cb7269bb0e523b4
Adam <N00211418@iadt.ie>
2023-02-09 00:44:53 +0000

    py chart working

--- Pychart/sketch.js
+++ Pychart/sketch.js
@@ -1,7 +1,10 @@
 const initialData = [
-  { name: 'cars', value: 33 },
-  { name: 'bus', value: 90 },
-  { name: 'yes', value: 180 },
+  { name: 'cars', value: 25 },
+  { name: 'buses', value: 25 },
+  { name: 'buses', value: 25 },
+  { name: 'buses', value: 25 },
+  { name: 'buses', value: 25 },
+  { name: 'buses', value: 25 },
 ]
 
 const data = getPercent(initialData)
@@ -22,33 +25,48 @@ function getPercent(data) {
     return accumulator + object.value;
   }, 0);
 
-  console.log(sum)
-
   data.forEach(element => {
 
     // should use class / contructor here
     let current = {
       name: element.name,
       percent: (element.value / sum) * 100,
+      color: (this.percent / 255) * 100
     }
 
     final.push(current)
   }, 0);
 
-  console.log(final)
   return final
 }
 
 
 function drawSegments(data) {
 
+  let rotations = []
+
   // draw first segment in same position every time
   const firstSeg = (data[0].percent / 100) * 360
+  rotations.push(firstSeg)
   push()
-  fill(0)
+  fill(0, 255, 0)
   arc(0, 0, pieX, pieY, 0, firstSeg)
   pop()
 
+
+  for (let i = 1; i < data.length; i++) {
+    push()
+    fill(random(100, 255), 0, 0)
+    const currentSeg = (data[i].percent / 100) * 360
+    const prevRotations = rotations.reduce((accumulator, object) => {
+      return accumulator + object;
+    })
+
+    rotations.push(currentSeg)
+    rotate(prevRotations)
+    arc(0, 0, pieX, pieY, 0, currentSeg)
+    pop()
+  }
 
 }
 
@@ -58,13 +76,16 @@ function setup() {
   background(200);
   angleMode(DEGREES);
   getPercent(initialData)
+  noStroke()
+  noLoop();
+
 }
 
 
 function draw() {
+
   translate(transX, transY)
   ellipse(0, 0, pieX, pieY)
-
   drawSegments(data)
 
 }
