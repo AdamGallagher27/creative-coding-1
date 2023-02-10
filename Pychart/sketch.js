73c64b58da7d739f4507956801124e2995fd14f9
Adam <N00211418@iadt.ie>
2023-02-10 23:12:51 +0000

    refactored / tidied code

--- Pychart/sketch.js
+++ Pychart/sketch.js
@@ -1,8 +1,13 @@
 // initial data
 const initialData = [
-  { name: '1', value: 100 },
-  { name: '2', value: 5 },
-  { name: '3', value: 5 },
+  { name: 'Adam', value: 80 },
+  { name: 'Niamh', value: 40 },
+  { name: 'Sarah', value: 10},
+  { name: 'Kate', value: 30 },
+  { name: 'Brian', value: 100 },
+  { name: 'Sean', value: 20 },
+  { name: 'Holly', value: 10 },
+  { name: 'Eleanor', value: 300 }
 ]
 
 // clean data
@@ -15,16 +20,23 @@ const transX = 250
 const transY = 250
 const pieX = 250
 const pieY = 250
-const maxData = Math.max(...initialData.map(obj => obj.value));
+const maxData = Math.max(...initialData.map(obj => obj.value))
+const textMargin = 165
 
-
+// holds all angles of previous rotations
+let rotations = []
 
 // function for converting the value to a percentage
 // ...ie cleaning
 function getPercent(data) {
 
+  // final to be returned at the end
   let final = []
+
+  // varaible for the color scale 
   const colorScale = scaleColor()
+
+  // sum of all the values in inital data
   const sum = data.reduce((accumulator, object) => {
     return accumulator + object.value;
   }, 0);
@@ -33,77 +45,40 @@ function getPercent(data) {
   data.forEach((element, index) => {
 
     // should use class / contructor here
+    // restructuring intial data to be a percentage and have a color
     let current = {
       name: element.name,
       percent: (element.value / sum) * 100,
       color: colorScale * (index + 1)
     }
 
+    // add them to final
     final.push(current)
   }, 0);
 
-  console.log(final)
+  // return the new data in a sorted array
   return final.sort((a, b) => a.percent - b.percent)
 }
 
+
 // function that draws each segment of the chart
-function drawPie(data) {
+function makePie(data) {
 
-  // holds all angles of previous rotations
-  let rotations = []
+  // draw the first segment in the same place every time
   const firstData = data[0]
-
-  // draw first segment in same position every time
-  const firstSeg = (firstData.percent / 100) * 360
-  const firstUpRight = makeUpRight(0, firstSeg)
-
-  // add rotations to rotations array
-  rotations.push(firstSeg)
-
-  
-  push()
-  // asign colour and draw arc with first sec angle
-  fill(0, firstData.color, 200)
-  arc(0, 0, pieX, pieY, 0, firstSeg)
-  rotate(firstSeg/2)
-  textAlign(CENTER)
-  translate(165, 0)
-  rotate(firstUpRight)
-  text(firstData.name + ` (${firstData.percent.toFixed(1)}%)`, 0, 0)
-  pop()
+  drawSegment(firstData)
 
   // loop through the rest of the data
   for (let i = 1; i < data.length; i++) {
     push()
 
-    fill(0, data[i].color, 200)
-
-    // current segment / the sum of all previous rotations
-    const currentSeg = (data[i].percent / 100) * 360
+    // the sum of all previous rotations
     const prevRotations = rotations.reduce((accumulator, object) => {
       return accumulator + object;
     })
-    
-    // add current segment to rotations
-    rotations.push(currentSeg)
-    
-    // rotate the sum of previous rotations then draw the arc of current seg
-    rotate(prevRotations)
-    arc(0, 0, pieX, pieY, 0, currentSeg)
 
-    // variable for setting the text to be upright 
-    const upRight = makeUpRight(prevRotations, currentSeg)
-    
-    // rotate each label to the center of its segment
-    rotate(currentSeg / 2)
-
-    // draw label code
-    push()
-    textAlign(CENTER);
-    translate(165, 0)
-    rotate(upRight)
-    text(data[i].name + ` (${data[i].percent.toFixed(1)}%)`, 5, 5)
-    pop()
+    // draw the segment for the current
+    drawSegment(data[i], prevRotations)
     pop()
   }
 
@@ -111,7 +86,43 @@ function drawPie(data) {
 }
 
 
-function makeUpRight(rotation, segment ) {
+function drawSegment(data, prevRotat=0) {
+
+  // current segment / varaible to make text sit upright
+  const currentSeg = (data.percent / 100) * 360
+  const upRight = makeUpRight(prevRotat, currentSeg)
+  
+  // add rotations to rotations array
+  rotations.push(currentSeg)
+
+  push()
+  // asign colour and draw arc with first sec angle
+  fill(0, data.color, 200)
+
+  // if the previous rotation exists rotate the grid by that total
+  if(prevRotat) {
+    rotate(prevRotat)
+  }
+
+  // draw the segment
+  arc(0, 0, pieX, pieY, 0, currentSeg)
+
+  // draw label for each segment
+  // rotate the text half of the current to get it in the middle
+  rotate(currentSeg / 2)
+  textAlign(CENTER)
+  translate(textMargin, 0)
+
+  // rotate by upright to make text upright
+  rotate(upRight)
+  text(data.name + ` (${data.percent.toFixed(1)}%)`, 0, 0)
+  pop()
+
+
+}
+
+// makes the text appear upright on piechart
+function makeUpRight(rotation, segment) {
   return (360 - rotation) - (segment / 2)
 }
 
@@ -135,10 +146,10 @@ function setup() {
 
 // draw 
 function draw() {
-  background(20)
+  background(30)
 
   // translate to draw from origin in center
-  // draw the segments
+  // draw the piechart
   translate(transX, transY)
-  drawPie(data)
+  makePie(data)
 }
