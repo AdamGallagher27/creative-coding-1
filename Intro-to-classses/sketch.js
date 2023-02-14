21764dd57c66b2b67f49c5611f66f837dd0ce324
Adam Gallagher (Student) <N00211418@iadt.ie>
2023-02-14 14:14:26 +0000

    created intro to classes

---
+++ Intro-to-classses/sketch.js
@@ -0,0 +1,17 @@
+
+let travel = [23, 56, 78, 90]
+
+
+function setup() {
+  createCanvas(screenWidth, screenHeight);
+  background(200);
+  angleMode(DEGREES);
+  rectMode(CORNER);
+  noLoop();
+}
+
+function draw() {
+  
+}
+
+
