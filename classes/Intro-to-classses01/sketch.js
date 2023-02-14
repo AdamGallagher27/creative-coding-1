ab596b232675558d83427727544e8d206b40bdfd
Adam Gallagher (Student) <N00211418@iadt.ie>
2023-02-14 17:00:51 +0000

    added intro to classes01

---
+++ classes/Intro-to-classses01/sketch.js
@@ -0,0 +1,25 @@
+
+let travel = [23, 56, 78, 90]
+
+
+function setup() {
+  createCanvas(500, 500);
+  background(200);
+  angleMode(DEGREES);
+  rectMode(CORNER);
+  noLoop();
+}
+
+function draw() {
+  
+}
+
+
+class BarChart {
+  constructor() {
+    this.height = 300 
+  }
+}
+
+
+let firstChart = new BarChart()
