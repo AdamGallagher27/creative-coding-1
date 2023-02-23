34bda4cfe3a469e4408f15426876a39b88764df7
Adam <N00211418@iadt.ie>
2023-02-23 20:32:05 +0000

    refactored code

--- Final/classes/StackedBarChart.js
+++ Final/classes/StackedBarChart.js
@@ -18,15 +18,16 @@ class StackedBarChart {
         this.marginB = 10
         this.marginAxisT = 65
         this.marginAxisL = 50
-        this.tickWidth = 10
+        this.tickWidth = 6
         this.nTicks = 4
+
+        // calculations
         this.blockWidth = (this.width - (this.marginL * 2) - ((this.nBlocks - 1))) / this.nBlocks
         this.mainGap = this.blockWidth + this.valGap
         this.maxVal = Math.max(...this.data.map(o => int(o.total)))
 
         // legend data
         this.legendData = {}
-
 
         // colors
         this.colors = ['#004c6d', '#4c7c9b', '#86b0cc', '#c1e7ff']
@@ -78,19 +79,22 @@ class StackedBarChart {
         if (lable) {
 
             // gaps between ticks
-            let tGap = this.height / this.nTicks
+            const tGap = this.height / this.nTicks
 
             // value to display beside each tick
-            let numGap = this.maxVal / this.nTicks
+            const numGap = this.maxVal / this.nTicks
+
+            // margin left for the tick
+            const tickMargin = -this.tickWidth - 6
 
             // draw each tick
             for (let i = 0; i <= this.nTicks; i++) {
                 noStroke()
                 textAlign(RIGHT, CENTER)
                 fill(0)
-                text(i * numGap.toFixed(0), -this.tickWidth, i * -tGap)
+                text(i * numGap.toFixed(0), tickMargin, i * -tGap)
                 stroke(100)
-                line(0, i * -tGap, -6, -i * tGap)
+                line(0, i * -tGap, -this.tickWidth, -i * tGap)
             }
         }
     }
@@ -107,6 +111,7 @@ class StackedBarChart {
         // draw each bar
         for (let i = this.data.length - 1; i > -1; i--) {
 
+            // move bar over to next position
             push();
             translate(this.marginL + (i * this.mainGap), 0)
 
@@ -114,21 +119,29 @@ class StackedBarChart {
             let colorIndex = 0
             
             // loop over keys in current object 
-            for(const key in current) {
+            for(const value in current) {
                 // for some reason || wont work
                 // double if is bad but works for now
-                // if key is not sales men or total draw bar and add colour value to obj
-                if (key !== 'total') {
-                    if (key !== 'salesmen') {
+                // if value is not sales men or total draw bar and add colour value to obj
+                if (value !== 'total') {
+                    if (value !== 'age_group') {
 
+                        // create current colour and make it the fill
                         const col = this.colors[colorIndex]
                         fill(col)
-                        rect(0, 0, this.blockWidth, -int(current[key]) * scaleValue)
-                        translate(0, -int(current[key]) * scaleValue)
+
+                        // the block height is the value in current object
+                        const blockHeight = -int(current[value]) * scaleValue
+
+                        // draw a bar with blockwidth and the blockheight
+                        // translate up blockheight
+                        rect(0, 0, this.blockWidth, blockHeight )
+                        translate(0, blockHeight)
+
                         colorIndex += 1
 
-                        // add current key / current color to legend data
-                        this.legendData[key] = col
+                        // add current value / current color to legend data
+                        this.legendData[value] = col
                     }
 
                 }
@@ -147,16 +160,21 @@ class StackedBarChart {
 
         // used to hold the texts y value
         let textPos = 0
-
+        
+        // varaibles for positioning / spacing
+        const textMargin = 40
+        const circleMargin = 30
+        const circleSize = 8
+        const lineSpacing = 15
 
         for (const property in obj) {
             fill(0)
-            text(property, this.width + 40, -this.height - textPos)
+            text(property, this.width + textMargin, -this.height - textPos)
             fill(obj[property])
-            ellipse(this.width + 30, -this.height - textPos, 8, 8)
+            ellipse(this.width + circleMargin, -this.height - textPos, circleSize)
 
             // move the text down a line
-            textPos -= 15
+            textPos -= lineSpacing
         }
 
     }
@@ -166,22 +184,31 @@ class StackedBarChart {
         fill(0)
         textAlign(RIGHT, CENTER)
 
+        // text angle / margin
+        const titleMargin = 10
+        const textAngle = -50
+
         // for every title draw the title 
         // half way through every block
         for (let i = 0; i < this.nBlocks; i++) {
 
             const currentGap = i * this.mainGap
             push()
-            translate((currentGap + this.marginL) + (this.blockWidth / 2), 10)
-            rotate(-50)
-            text(data[i].salesmen, 0, 0)
+            translate((currentGap + this.marginL) + (this.blockWidth / 2), titleMargin)
+            rotate(textAngle)
+            text(data[i].age_group, 0, 0)
             pop()
         }
     }
 
     // draws the titles for each axis
     axisTitles() {
-        textSize(18)
+
+        // size / rotation
+        const size = 18
+        const rotation = -90
+
+        textSize(size)
         textAlign(CENTER)
         noStroke()
 
@@ -189,7 +216,7 @@ class StackedBarChart {
         text(this.xLable, this.width / 2, this.marginAxisT)
 
         // Y axis lable
-        rotate(-90)
+        rotate(rotation)
         text(this.yLable, this.height / 2, -this.marginAxisL)
     }
 
