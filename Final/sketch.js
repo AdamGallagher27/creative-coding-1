c878877b996888e5c674c419b6731dd895febc31
Adam <N00211418@iadt.ie>
2023-03-09 21:04:35 +0000

    added alt data

--- Final/sketch.js
+++ Final/sketch.js
@@ -14,6 +14,9 @@
 // scatter chart =
 // number of kidney transplants by age
 
+// stacked hundred = 
+// amount of alternate surgeries from year 2014 to 2020
+
 
 // instanciate variables for csv data
 let digital
@@ -26,6 +29,7 @@ let barData
 let donutData
 let horData
 let scatterData
+let hundData
 
 
 function preload() {
@@ -69,6 +73,13 @@ function preload() {
 		// console.log(scatterData)
 	})
 
+	// load transplant data from csv and return as array of objects
+	let hundred = loadTable('./data/transplant-data/hund-data.csv', 'csv', 'header', () => {
+		hundData = Object.values(hundred.getObject())
+		console.log("hundred data")
+		console.log(hundData)
+	})
+
 }
 
 
@@ -81,7 +92,7 @@ const globalTitleSize = 25
 const doTitle = 'liver transplants by age group (35-45 to 65-75)'
 const doWidth = 200
 const doHeight = 200
-const doPosX = 700
+const doPosX = 800
 const doPosy = 350
 
 
@@ -89,26 +100,26 @@ const doPosy = 350
 const barChartTitle = 'amount of kidney, lung, heart and liver transplants in ireland 2021'
 const barChartWidth = 300
 const barChartHeight = 200
-const barChartPosX = 70
+const barChartPosX = 170
 const barChartPosy = 450
 const barChartXLable = "type of transplants"
 const barChartYLable = "num transplants"
 
 
-// bar chart global variables
+// scatter chart global variables
 const scatterChartTitle = 'number of kidney transplants by age'
 const scatterChartWidth = 300
 const scatterChartHeight = 200
-const scatterChartPosX = 1100
+const scatterChartPosX = 1200
 const scatterChartPosy = 450
 const scatterChartXLable = "age range"
 const scatterChartYLable = "num transplants"
 
 
 // stacked bar chart
-const stackedWidth = 200
-const stackedHeight = 300
-const stackedPosX = 70
+const stackedWidth = 300
+const stackedHeight = 200
+const stackedPosX = 170
 const stackedPosY = 900
 const stackedXLable = 'age range'
 const stackedYLable = 'number transplants'
@@ -116,26 +127,27 @@ const stackedTitle = 'all transplants by age'
 
 
 // stacked 100% bar chart
-const hundWidth = 200
+const hundWidth = 400
 const hundHeight = 300
-const hundPosX = 1100
-const hundPosY = 900
-const hundXLable = 'age range'
-const hundYLable = 'number transplants'
+const hundPosX = 1220
+const hundPosY = 950
+const hundTitle = 'type of surgeries by year'
+const hundXLable = 'years'
+const hundYLable = 'opperations'
 
 
 // horizontal bar chart global variables
 const horTitle = 'kidney transplants by year (2017 - 2021)'
 const horBarChartWidth = 350
 const horBarChartHeight = 200
-const horBarChartPosX = 550
+const horBarChartPosX = 690
 const horBarChartPosy = 900
 const horBarChartXLable = "num kidney transplants"
 const horBarChartYLable = "years"
 
 
 // screen dimensions
-const screenWidth = 1500
+const screenWidth = 1800
 const screenHeight = 1500
 const bg = 240
 const marginT = globalTitleSize
@@ -157,6 +169,9 @@ function setup() {
 	textSize(globalTextSize)
 
 
+	// the bar, horizontal, donut and scatter charts can take any csv data as long as it has two values in it
+	// this means there data variables can be switched and the chart will work fine
+
 	// bar chart
 	const barChart = new BarChart(barChartHeight, barChartWidth, barChartPosX, barChartPosy, barData, barChartXLable, barChartYLable, barChartTitle)
 	barChart.render()
@@ -165,21 +180,59 @@ function setup() {
 	const donutChart = new Donut(doWidth, doHeight, doPosX, doPosy, donutData, doTitle)
 	donutChart.render()
 
-	// stacked bar chart
-	const stackedBarChart = new StackedBarChart(stackedWidth, stackedHeight, stackedPosX, stackedPosY, stackedData, stackedXLable, stackedYLable, stackedTitle)
-	stackedBarChart.render()
-
-	// 100 % stacked bar chart
-	const stackedHundred = new StackedHund(hundWidth, hundHeight, hundPosX, hundPosY, stackedData, hundXLable, hundYLable, stackedTitle)
-	stackedHundred.render()
-
+	
 	// horizotal bar chart
 	const horBarChart = new HorBarChart(horBarChartHeight, horBarChartWidth, horBarChartPosX, horBarChartPosy, horData, horBarChartXLable, horBarChartYLable, horTitle)
 	horBarChart.render()
-
-	// bar chart
+	
+	// scatter chart
 	const scatterChart = new ScatterChart(scatterChartHeight, scatterChartWidth, scatterChartPosX, scatterChartPosy, scatterData, scatterChartXLable, scatterChartYLable, scatterChartTitle)
 	scatterChart.render()
+	
+	// the stacked / 100% stacked chart work with any csv data as long as it fits the specified shape 
+	// this means stacked and 100% stacked data can be switched and the charts will work fine
+
+	// stacked bar chart
+	const stackedBarChart = new StackedBarChart(stackedHeight, stackedWidth , stackedPosX, stackedPosY, stackedData, stackedXLable, stackedYLable, stackedTitle)
+	stackedBarChart.render()
+
+	// 100 % stacked bar chart
+	const stackedHundred = new StackedHund(hundHeight, hundWidth, hundPosX, hundPosY, hundData, hundXLable, hundYLable, hundTitle)
+	stackedHundred.render()
+
+
+
+
+
+
+
+	// these charts bellow are the same classes but with different data
+
+	// // bar chart
+	// const barChartAlt = new BarChart(barChartHeight, barChartWidth, barChartPosX, barChartPosy, scatterData, barChartXLable, barChartYLable, barChartTitle)
+	// barChartAlt.render()
+
+	// // donut chart
+	// const donutChartAlt = new Donut(doWidth, doHeight, doPosX, doPosy, barData, doTitle)
+	// donutChartAlt.render()
+
+	
+	// // horizotal bar chart
+	// const horBarChartAlt = new HorBarChart(horBarChartHeight, horBarChartWidth, horBarChartPosX, horBarChartPosy, donutData, horBarChartXLable, horBarChartYLable, horTitle)
+	// horBarChartAlt.render()
+	
+	// // scatter chart
+	// const scatterChartAlt = new ScatterChart(scatterChartHeight, scatterChartWidth, scatterChartPosX, scatterChartPosy, horData, scatterChartXLable, scatterChartYLable, scatterChartTitle)
+	// scatterChartAlt.render()
+
+	// // stacked bar chart
+	// const stackedBarChartAlt = new StackedBarChart(stackedHeight, stackedWidth , stackedPosX, stackedPosY, hundData, stackedXLable, stackedYLable, stackedTitle)
+	// stackedBarChartAlt.render()
+
+	// // 100 % stacked bar chart
+	// const stackedHundredAlt = new StackedHund(hundHeight, hundWidth, hundPosX, hundPosY, stackedData, hundXLable, hundYLable, hundTitle)
+	// stackedHundredAlt.render()
+
 
 }
 
